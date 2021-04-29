414b8c96b9c0a6fe7702003c772ecd023f0ebd2d
Tarun-Mahajan <tiit828@gmail.com>
2021-04-29 01:46:57 -0500

    Add doxygen comments for graph header file
    Add doxygen comments for member variables and functions

--- src/GRN_simulation/graph.hpp
+++ src/GRN_simulation/graph.hpp
@@ -9,45 +9,97 @@ struct Edge {
 
 class Graph {
   /* Data */
-  // Number of nodes in the Graph.
+  /********************************************//**
+   \brief Number of nodes in the graph.
+   ***********************************************/
   int num_nodes;
 
-  // A vector or vectors to represent adjacency list.
+  /********************************************//**
+   \brief A vector of vectors to represent adjacency list.
+   ***********************************************/
   std::vector<std::vector<int>> adj_list;
 
   /* Memeber functions */
-  // Recursive DFS function for checking graph connectedness.
+  /********************************************//**
+   \brief Recursive DFS function for checking graph connectedness.
+
+   \param[in] vert vertex in the graph from where the DFS is initiated.
+   \param[in] visited boolean vector to track whether vertices in the
+              graph have been visited by the DFS or not.
+   ***********************************************/
   void is_connected_DFS (int vert, bool visited[]);
 
   // Make graph undirected.
+  /********************************************//**
+   \brief Make graph undirected for checking weak connectedness.
+
+   \param[in] vector of edges to store edges added to make the graph
+              undirected.
+   ***********************************************/
   void make_undirected (std::vector<Edge> &edges);
 
-  // Unmake graph undirected.
+  /********************************************//**
+   \brief Unmake graph undirected after checking for weak connectedness.
+
+   \param[in] vector of edges to store edges to delete
+              to make the graph directed again.
+   ***********************************************/
   void unmake_undirected (std::vector<Edge> &edges);
 
   // Recursive DFS utility function for determining whether graph is DAG or not.
+  /********************************************//**
+   \brief Recursive DFS utility function for determining whether
+          graph is DAG or not.
+
+   \param[in] vert vertex in the graph from where the DFS is initiated.
+   \param[in] visited boolean vector to track whether vertices in the
+              graph have been visited by the DFS or not.
+   ***********************************************/
   bool is_DAG_util (int vert, bool visited[], bool active[]);
 
 
 
 public:
   /* Memeber functions */
-  // Graph constructor.
+  /********************************************//**
+   \brief Graph Constructor.
+
+   \param[in] N Number of nodes in the graph.
+   ***********************************************/
   Graph (int N);
 
-  // Function to add edge.
+  /********************************************//**
+   \brief Function to add edge to the graph.
+
+   \param[in] src source vertex for the edge.
+   \param[in] dest destination vertex for the edge.
+   ***********************************************/
   void add_edge (int src, int dest);
 
-  // Function to delete edge.
+  /********************************************//**
+   \brief Function to delete edge from the graph.
+
+   \param[in] src source vertex for the edge.
+   \param[in] dest destination vertex for the edge.
+   ***********************************************/
   void del_edge (int src, int dest);
 
-  // Function to check whether graph is connected or not.
+  /********************************************//**
+   \brief Function to check whether graph is weakly connected or not.
+   ***********************************************/
   bool is_connected ();
 
-  // Function to check whether graph is DAG or not.
+  /********************************************//**
+   \brief Function to check whether graph is DAG or not.
+   ***********************************************/
   bool is_DAG ();
 
-  // Function to return children nodes of a given node.
+  /********************************************//**
+   \brief Function to return children nodes of a given node.
+
+   \param[in] vert vertex to return children for.
+   \param[in] children vector to store the children nodes.
+   ***********************************************/
   void find_children (int vert, std::vector<int> &children);
 };
 

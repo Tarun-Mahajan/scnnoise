b28b176d613b4b39e4ea538d94a3cbc0e5891bc6
Tarun-Mahajan <tiit828@gmail.com>
2021-05-31 12:34:34 -0500

    Implement simulate method
    Implement the sorting direct method for Gillespie's
    stochastic simulation algorithm.

--- src/gillespie/gillespieSDM.cpp
+++ src/gillespie/gillespieSDM.cpp
@@ -1,25 +1,255 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <omp.h>
+#include <string>
+#include <random>
+#include <math.h>
 #include "../GRN_simulation/graph.hpp"
-#include "gillespie.hpp"
+#include "../scnnoise.hpp"
+#include "gillespieSDM.hpp"
 
 /* function definitions */
 // Constructor
-Gillespie::Gillespie (int num_rxns, int num_species):: Graph (num_rxns){
+GillespieSDM::GillespieSDM (int num_rxns, int num_species, int num_nodes_GRN):
+  scnnoise (num_rxns, num_species, num_nodes_GRN){
+  const int sz = 1000;
   this->num_rxns = num_rxns;
   this->num_species = num_species;
   time = 0;
-  molecule_count_cur.resize(num_species);
+  molecule_count_cur.reserve(sz);
   save_timeseries = false;
-  num_timepoints_save = 100;
+  num_timepoints_save = 1000;
+  time_history.reserve(int(5*sz));
   time_history.push_back(0);
-  rxn_rates.resize(num_rxns);
-  rxn_stoi.resize(num_rxns);
-  rxn_propensity.resize(num_rxns);
-  rxn_freq.resize(num_rxns);
+  rxn_rates.reserve(sz);
+  rxn_stoi.reserve(sz);
+  rxn_propensity.reserve(sz);
+  rxn_freq.reserve(sz);
 }
 
-Gillespie::~Gillespie () {
+inline double GillespieSDM::sample_time_step (RNG &generator) {
+  thread_local std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  double rand_num = distribution(generator);
+  return -log(1 - rand_num)/total_propensity;
+}
 
+int GillespieSDM::sample_next_rxn (RNG &generator) {
+  thread_local std::uniform_real_distribution<double> distribution(0.0, 1.0);
+  double rand_num = distribution(generator);
+  double selector = total_propensity * (double(1.0) - rand_num);
+  int rxn_selected = -1;
+
+  for ( int i = 0; i < num_rxns; ++i) {
+    selector -= rxn_order[i].propensity_val;
+    if (selector <= 0) {
+      rxn_selected = i;
+      break;
+    }
+  }
+  if (rxn_selected > 0) {
+    std::swap(rxn_order[rxn_selected - 1], rxn_order[rxn_selected]);
+    rxn_selected -= 1;
+  }
+  return rxn_selected;
+}
+
+void GillespieSDM::update_fired_Reaction (int rxn_selected, bool &GRN_out_changed) {
+  /*
+  Find the gene and reaction type for the selected reaction channel. Extract the reactants,
+  products and their stoichiometric coefficients for the selected reaction channel.
+  */
+  int gene_selected = rxn_order[rxn_selected].gene_id;
+  // int rxn_type_selected = rxn_order[rxn_selected].rxn_type;
+  int rxn_index = rxn_order[rxn_selected].rxn_type;
+  // std::vector<int> rxn_temp = reactions[gene_selected].rxn_type;
+  // std::vector<int>::iterator it = std::find(rxn_temp.begin(), rxn_temp.end(),
+  //   rxn_type_selected);
+  // int rxn_index = std::distance(rxn_temp.begin(), it);
+  std::vector<int> rxn_selected_reactants =
+    reactions[gene_selected].rxns[rxn_index].reactants;
+  std::vector<int> rxn_selected_products =
+    reactions[gene_selected].rxns[rxn_index].products;
+  std::vector<int> rxn_selected_reactants_stoichio =
+    reactions[gene_selected].rxns[rxn_index].reactants_stoichio;
+  std::vector<int> rxn_selected_products_stoichio =
+    reactions[gene_selected].rxns[rxn_index].products_stoichio;
+
+  /*
+  Subtract current reaction propensity from total reaction propensity for
+  computing updated total propensity later.
+  */
+  total_propensity -= reactions[gene_selected].rxns[rxn_index].propensity_val;
+
+  /*
+  Update molecular count for reactants which change by firing the selected
+  reaction channel.
+  */
+  int count_not_changed_reactants = 0;
+  vector<bool> flag_changed_product_count(rxn_selected_products.size(), false);
+  for (auto r = rxn_selected_reactants.begin(); r != rxn_selected_reactants.end(); ++r) {
+    int reactant_index = std::distance(rxn_selected_reactants.begin(), r);
+    std::vector<int>::iterator it =
+    std::find(rxn_selected_products.begin(), rxn_selected_products.end(),
+    *r);
+    if (it != rxn_selected_products.end()) {
+      int product_index = std::distance(rxn_selected_products.begin(), it);
+      flag_changed_product_count[product_index] = true;
+      int count_change = rxn_selected_products_stoichio[product_index] -
+        rxn_selected_reactants_stoichio[reactant_index]
+      if (count_change) {
+        reactions[gene_selected].molecule_count_cur[*r] += count_change;
+        if (*r == reactions[gene_selected].GRN_rxn_OUT) {
+          GRN_out_changed = true;
+        }
+      }else{
+        count_not_changed_reactants += 1;
+      }
+    }else{
+      reactions[gene_selected].molecule_count_cur[*r] -=
+        rxn_selected_reactants_stoichio[reactant_index];
+    }
+  }
+
+  /*
+  Update molecular count for products which change by firing the selected
+  reaction channel.
+  */
+  for (auto r = rxn_selected_products.begin(); r != rxn_selected_products.end(); ++r) {
+    int product_index = std::distance(rxn_selected_products.begin(), r);
+    if (!flag_changed_product_count[product_index]) {
+      reactions[gene_selected].molecule_count_cur[*r] +=
+        rxn_selected_products_stoichio[product_index];
+        if (*r == reactions[gene_selected].GRN_rxn_OUT) {
+          GRN_out_changed = true;
+        }
+    }
+  }
+
+  /*
+  Update propensity for the selected reaction channel.
+  */
+  if (count_not_changed_reactants < rxn_selected_reactants.size()) {
+    double new_propensity = reactions[gene_selected].rxns[rxn_index].rxn_rate;
+    for (auto r = rxn_selected_reactants.begin(); r != rxn_selected_reactants.end(); ++r) {
+      int reactant_index = std::distance(rxn_selected_reactants.begin(), r);
+      int reactant_stoichio = rxn_selected_reactants_stoichio[reactant_index];
+      new_propensity *= (factorial(reactions[gene_selected].molecule_count_cur[*r])/
+        factorial(reactions[gene_selected].molecule_count_cur[*r] - reactant_stoichio));
+    }
+    if (rxn_index == reactions[gene_selected].GRN_rxn_IN) {
+      new_propensity *= regulation_function(gene_selected);
+    }
+    reactions[gene_selected].rxns[rxn_index].propensity_val = new_propensity;
+    rxn_order[rxn_selected].propensity_val = new_propensity;
+
+  }
+  total_propensity += reactions[gene_selected].rxns[rxn_index].propensity_val;
+}
+
+void GillespieSDM::update_dependent_count_propensity (int rxn_selected,
+  bool &GRN_out_changed) {
+
+    /*
+    Update propensity for dependent reactions belonging to the same gene as the
+    reaction selected for firing
+    */
+    int gene_selected = rxn_order[rxn_selected].gene_id;
+    int rxn_index = rxn_order[rxn_selected].rxn_type;
+    int gene_type = reactions[gene_selected].gene_type;
+    vector<int> rxn_selected_children;
+    rxn_selected_children.reserve(gene_rxn_dependency[gene_type].get_size());
+    gene_rxn_dependency[gene_type].find_children(rxn_index, rxn_selected_children);
+    std::vector<int> rxn_selected_reactants;
+    rxn_selected_reactants.reserve(reactions[gene_selected].molecule_count_cur.size());
+    std::vector<int> rxn_selected_reactants_stoichio;
+    rxn_selected_reactants_stoichio.reserve(reactions[gene_selected].molecule_count_cur.size());
+
+    for (auto &rxn : rxn_selected_children) {
+      total_propensity -= reactions[gene_selected].rxns[rxn].propensity_val;
+      rxn_selected_reactants =
+        reactions[gene_selected].rxns[rxn].reactants;
+      rxn_selected_reactants_stoichio =
+        reactions[gene_selected].rxns[rxn].reactants_stoichio;
+
+      double new_propensity = reactions[gene_selected].rxns[rxn].rxn_rate;
+      for (auto r = rxn_selected_reactants.begin(); r != rxn_selected_reactants.end(); ++r) {
+        int reactant_index = std::distance(rxn_selected_reactants.begin(), r);
+        int reactant_stoichio = rxn_selected_reactants_stoichio[reactant_index];
+        new_propensity *= (factorial(reactions[gene_selected].molecule_count_cur[*r])/
+          factorial(reactions[gene_selected].molecule_count_cur[*r] - reactant_stoichio));
+      }
+      if (rxn == reactions[gene_selected].GRN_rxn_IN) {
+        new_propensity *= regulation_function(gene_selected);
+      }
+      reactions[gene_selected].rxns[rxn].propensity_val = new_propensity;
+      for (auto &r : rxn_order) {
+        if ((r.gene_id == gene_selected) && (r.rxn_type == rxn)) {
+          r.propensity_val = new_propensity;
+          break;
+        }
+      }
+      total_propensity += reactions[gene_selected].rxns[rxn].propensity_val;
+
+    }
+
+
+    /*
+    Update propensity for dependent reactions belonging to genes
+    downstream of the gene related to the reaction selected for firing
+    */
+    if (GRN_out_changed) {
+      vector<int> gene_children;
+      network[0].find_children(gene_selected, gene_children);
+      for (auto &g : gene_children) {
+        int rxn = reactions[g].GRN_rxn_IN;
+        total_propensity -= reactions[g].rxns[rxn].propensity_val;
+        rxn_selected_reactants =
+          reactions[g].rxns[rxn].reactants;
+        rxn_selected_reactants_stoichio =
+          reactions[g].rxns[rxn].reactants_stoichio;
+
+        double new_propensity = reactions[g].rxns[rxn].rxn_rate;
+        for (auto r = rxn_selected_reactants.begin(); r != rxn_selected_reactants.end(); ++r) {
+          int reactant_index = std::distance(rxn_selected_reactants.begin(), r);
+          int reactant_stoichio = rxn_selected_reactants_stoichio[reactant_index];
+          new_propensity *= (factorial(reactions[g].molecule_count_cur[*r])/
+            factorial(reactions[g].molecule_count_cur[*r] - reactant_stoichio));
+        }
+        new_propensity *= regulation_function(g);
+        reactions[g].rxns[rxn].propensity_val = new_propensity;
+        for (auto &r : rxn_order) {
+          if ((r.gene_id == g) && (r.rxn_type == rxn)) {
+            r.propensity_val = new_propensity;
+            break;
+          }
+        }
+        total_propensity += reactions[g].rxns[rxn].propensity_val;
+      }
+    }
+}
+
+double GillespieSDM::simulate (int num_threads) {
+  compute_total_propensity();
+
+  std::vector<std::uint_least32_t> rd_seeds = {rd(), rd(), rd(), rd()};
+  std::seed_seq sd(rd_seeds.begin(), rd_seeds.end());
+  thread_local static RNG generator{sd};
+  bool stop_sim = false;
+
+  while (!stop_sim) {
+    double next_time_step = sample_time_step(generator);
+    int next_rxn = sample_next_rxn(generator);
+    bool GRN_out_changed = false;
+    double total_time = std::accumulate(time_history.begin(), time_history.end(),
+      decltype(time_history)::value_type(0)) + next_time_step;
+    if (total_time < max_time) {
+      update_fired_Reaction(next_rxn, GRN_out_changed);
+      update_dependent_count_propensity(next_rxn, GRN_out_changed);
+      time_history.push_back(next_time_step);
+
+    }else{
+      stop_sim = true;
+    }
+  }
 }
--- src/gillespie/gillespieSDM.hpp
+++ src/gillespie/gillespieSDM.hpp
@@ -9,26 +9,9 @@
  reaction network.
  ***********************************************/
 
-class Gillespie: public Graph {
+class GillespieSDM: public scnnoise {
 private:
   /* data */
-  /********************************************//**
-  \brief Number of chemical reaction channels.
-
-  This is the total number of unique chemical
-  raction channels that are present in the  user
-  provided chemical reaction network.
-   ***********************************************/
-  int num_rxns;
-
-  /********************************************//**
-  \brief Number of molecular species.
-
-  This is the total number of unique molecular
-  species that are present in the user provided
-  chemical reaction network.
-   ***********************************************/
-  int num_species;
 
   /********************************************//**
   \brief Time period for simulation run.
@@ -38,16 +21,6 @@ private:
    ***********************************************/
   double time;
 
-  // Vector of current molecule counts for all species.
-  /********************************************//**
-   \brief Vector of current molecule counts for all species.
-
-   This stores the current molecule counts for all
-   species in the chemical reaction network along
-   the simulation trajectory.
-   ***********************************************/
-  std::vector<int> molecule_count_cur;
-
   /********************************************//**
    \brief Save time series count data.
 
@@ -56,52 +29,64 @@ private:
    is false, when only steady state count values are
    reported.
    ***********************************************/
-
   bool save_timeseries;
 
-  // Number of previous time points for which molecular counts are stored.
+  /********************************************//**
+   \brief Number of previous time points for which molecular counts are stored in
+          a vector of vectors..
+
+   If save_timeseries is set to True, num_timepoints_save gives the number of
+   past values of molecular counts to store in an 2D array
+   (vector of vectors) of size . Once num_timepoints_save past values have been
+   stored for each moleulcar species, the contents of the array are
+   appened to an output file, and the array is emptied. After this,
+   array starts storing the futures values of molecular counts until
+   num_timepoints_save are stored, when the values are again appened to the
+   output file. This process continues until the simulation terminates.
+   At termination, the array again appends its values to the output file.
+   ***********************************************/
   int num_timepoints_save;
 
-  // Vector of vectors to store running molecule counts for all species for last 'num_timepoints_save' time points.
+  /********************************************//**
+   \brief Vector of vectors to store running molecule counts for all species
+          for last 'num_timepoints_save' time points.
+
+   A 2D array stored as a vector of vectors to store running molecular counts
+   for all the molecular specis in the system. This array ensures that the
+   molecular counts are written to an output file at a fixed interval. If
+   num_timepoints_save > 1, This reduces the number of times data has to be
+   written to the output file.
+   ***********************************************/
   std::vector<std::vector<int>> molecule_count_history;
 
-  // Vector for time points along the simulation path.
+  /********************************************//**
+   \brief Vector to store time points along the simulation path.
+   ***********************************************/
   std::vector<double> time_history;
 
-  // Vector of reaction rates for all rxn channels.
-  std::vector<double> rxn_rates;
+  int num_threads;
 
-  // Data structure to store stoichiometry info. for all rxn channels.
-  std::vector<map<int, map<int, std::vector<int>>>> rxn_stoi;
+  /********************************************//**
+   \brief Vector to store reaction propensities.
 
-  // Vector of rxn propensities for all reaction channels.
+   If \f$k\f$ is the reaction rate for a chemical reaction, then propensity,
+   \f$P = kN\f$, where \f$N\f$ is the number of different ways reactant species
+   can interact. \f$N\f$ is a function of the stoichiometric coefficients of the
+   reactants.
+   ***********************************************/
   std::vector<double> rxn_propensity;
 
-  // Vector for reaction firing frequency for all reaction channels.
-  std::vector<int> rxn_freq;
-
 public:
+  typedef std::mt19937 RNG;
   /* Member functions */
   // Constructor
-  Gillespie (int num_rxns, int num_species);
+  GillespieSDM (int num_rxns, int num_species, int num_nodes_GRN, double time,
+    bool save_timeseries, int num_timepoints_save
+    std::vector<std::vector<int>> molecule_count_history,
+    std::vector<double> time_history, std::vector<double> rxn_propensity);
 
   // Virtual destructor
-  virtual ~Gillespie ();
-
-  // Add reaction rates
-  void add_rxn_rate (int rxn_id, double rate);
-
-  // Add rxn input stoichiometric information.
-  void add_rxn_stoi_input (int rxn_id, int mol_id, int stoi);
-
-  // Add rxn output stoichiometric information.
-  void add_rxn_stoi_output (int rxn_id, int mol_id, int stoi);
-
-  // Update molecule count.
-  void update_molecule_count ();
-
-  // Update rxn propensity.
-  void update_propensity (int rxn_id);
+  virtual ~GillespieSDM ();
 
   // Sample next time step.
   double sample_time_step ();
@@ -109,20 +94,14 @@ public:
   // Sample next reaction id.
   int sample_next_rxn ();
 
+  // Update molecule counts by firing the selected reaction
+  void update_fired_Reaction (int rxn_selected, bool &GRN_out_changed);
+
+  // Update propensity for reactions dependent via the GRN
+  void update_dependent_count_propensity (int rxn_selected, bool &GRN_out_changed);
+
   // Simulate.
   void simulate ();
-
-  // Update reaction frequencies.
-  void update_rxn_freq (int next_rxn);
-
-  // Set time period for simulation run.
-  void set_time (double time);
-
-  // Initialize molecule counts.
-  void initialize_molecule_count (int species_id, int count);
-
-  // Set number of previous history counts to store.
-  void set_num_history (bool save_timeseries, int num_timepoints_save);
 };
 
 #endif

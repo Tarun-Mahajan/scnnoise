ea26e74b380e8d079abf0744c5a93dec238bebd5
Tarun-Mahajan <tiit828@gmail.com>
2021-06-23 00:30:58 -0500

    Update setup for cmake based install

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
+import os
+import re
+import sys
+import sysconfig
+import platform
+import subprocess
 from glob import glob
-from setuptools import setup
+from setuptools import setup, find_packages, Extension
+from setuptools.command.build_ext import build_ext
 
 # Available at setup time due to pyproject.toml
-from pybind11.setup_helpers import Pybind11Extension, build_ext, ParallelCompile
-from pybind11 import get_cmake_dir
+# from pybind11.setup_helpers import Pybind11Extension, build_ext, ParallelCompile
+# from pybind11 import get_cmake_dir
 
 # Optional multithreaded build
 # ParallelCompile("NPY_NUM_BUILD_JOBS").install()
 
-import sys
 
 __version__ = "0.0.1"
 
@@ -21,14 +27,99 @@ __version__ = "0.0.1"
 #   Sort input source files if you glob sources to ensure bit-for-bit
 #   reproducible builds (https://github.com/pybind/python_example/pull/53)
 
-ext_modules = [
-    Pybind11Extension("scnnoise",
-        # sorted(glob("src/*.cpp")),
-        sorted(glob(os.path.join('src', '*.cpp'))),
-        # Example: passing in the version to the compiled code
-        define_macros = [('VERSION_INFO', __version__)],
-        ),
-]
+# ext_modules = [
+#     Pybind11Extension("scnnoise",
+#         # sorted(glob("src/*.cpp")),
+#         sorted(glob(os.path.join('src', '*.cpp'))),
+#         # Example: passing in the version to the compiled code
+#         define_macros = [('VERSION_INFO', __version__)],
+#         ),
+# ]
+
+
+# A CMakeExtension needs a sourcedir instead of a file list.
+# The name must be the _single_ output extension from the CMake build.
+# If you need multiple extensions, see scikit-build.
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=""):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+
+        # required for auto-detection of auxiliary "native" libs
+        if not extdir.endswith(os.path.sep):
+            extdir += os.path.sep
+
+        cfg = "Debug" if self.debug else "Release"
+
+        # CMake lets you override the generator - we need to check this.
+        # Can be set with Conda-Build, for example.
+        cmake_generator = os.environ.get("CMAKE_GENERATOR", "")
+
+        # Set Python_EXECUTABLE instead if you use PYBIND11_FINDPYTHON
+        # EXAMPLE_VERSION_INFO shows you how to pass a value into the C++ code
+        # from Python.
+        cmake_args = [
+            "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}".format(extdir),
+            "-DPYTHON_EXECUTABLE={}".format(sys.executable),
+            "-DEXAMPLE_VERSION_INFO={}".format(self.distribution.get_version()),
+            "-DCMAKE_BUILD_TYPE={}".format(cfg),  # not used on MSVC, but no harm
+        ]
+        build_args = []
+
+        if self.compiler.compiler_type != "msvc":
+            # Using Ninja-build since it a) is available as a wheel and b)
+            # multithreads automatically. MSVC would require all variables be
+            # exported for Ninja to pick it up, which is a little tricky to do.
+            # Users can override the generator with CMAKE_GENERATOR in CMake
+            # 3.15+.
+            if not cmake_generator:
+                cmake_args += ["-GNinja"]
+
+        else:
+
+            # Single config generators are handled "normally"
+            single_config = any(x in cmake_generator for x in {"NMake", "Ninja"})
+
+            # CMake allows an arch-in-generator style for backward compatibility
+            contains_arch = any(x in cmake_generator for x in {"ARM", "Win64"})
+
+            # Specify the arch if using MSVC generator, but only if it doesn't
+            # contain a backward-compatibility arch spec already in the
+            # generator name.
+            if not single_config and not contains_arch:
+                cmake_args += ["-A", PLAT_TO_CMAKE[self.plat_name]]
+
+            # Multi-config generators have a different way to specify configs
+            if not single_config:
+                cmake_args += [
+                    "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)
+                ]
+                build_args += ["--config", cfg]
+
+        # Set CMAKE_BUILD_PARALLEL_LEVEL to control the parallel build level
+        # across all generators.
+        if "CMAKE_BUILD_PARALLEL_LEVEL" not in os.environ:
+            # self.parallel is a Python 3 only way to set parallel jobs by hand
+            # using -j in the build_ext call, not supported by pip or PyPA-build.
+            if hasattr(self, "parallel") and self.parallel:
+                # CMake 3.12+ only.
+                build_args += ["-j{}".format(self.parallel)]
+
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        subprocess.check_call(
+            ["cmake", ext.sourcedir] + cmake_args, cwd=self.build_temp
+        )
+        subprocess.check_call(
+            ["cmake", "--build", "."] + build_args, cwd=self.build_temp
+        )
+        print()  # Add an empty line for cleaner output
 
 setup(
     name="scnnoise",
@@ -36,18 +127,21 @@ setup(
     author="Tarun Mahajan",
     author_email="tarunm3@illinois.edu",
     url="https://github.com/Tarun-Mahajan/scnnoise",
-    description="Single cell Network-aware Noise Simulator for gene Expression",
+    description="ScNNoiSE: Single cell Network-aware Noise Simulator for gene Expression",
     long_description="A simulator for generating single-cell RNA sequencing data \
     using the two-state model of gene expression. Further, the simulator also \
-    takes account of gene expression noise propagation over gene regulatory \
-    network while generating the simulated data.",
+    takes account of gene expression noise propagation over gene regulatory network, \
+    cell-cycle fluctuation and extrinsic noise while generating the simulated data.",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    ext_modules=ext_modules,
     extras_require={"test": "pytest"},
     # Currently, build_ext only provides an optional "highest supported C++
     # level" feature, but in the future it may provide more features.
+    ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
+    ext_modules=[CMakeExtension('scnnoise/scnnoise')],
+    # add custom build_ext command
+    cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
     options={"bdist_wheel": {"universal": True}}
 )

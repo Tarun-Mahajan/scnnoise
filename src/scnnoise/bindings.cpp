22b8d48b43faf28ba5b39fd7d1794a82d96d1789
Tarun-Mahajan <tiit828@gmail.com>
2021-06-26 18:12:35 -0500

    Add system include path for pybind11 header files

--- src/scnnoise/bindings.cpp
+++ src/scnnoise/bindings.cpp
@@ -1,6 +1,7 @@
-// #include <pybind11/pybind11.h>
-#include "../../lib/pybind11/include/pybind11/pybind11.h"
-#include "../../lib/pybind11/include/pybind11/stl.h"
+#include <pybind11/pybind11.h>
+#include <pybind11/stl_bind.h>
+// #include "../../lib/pybind11/pybind11/include/pybind11/pybind11.h"
+// #include "../../lib/pybind11/pybind11/include/pybind11/stl.h"
 #include "scnnoise.hpp"
 #include "gillespieSSA.hpp"
 #include "gillespieSDM.hpp"

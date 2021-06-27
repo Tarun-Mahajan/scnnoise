60bde79d8a5b6ce817a2743c208638857c8640fb
Tarun-Mahajan <tiit828@gmail.com>
2021-06-26 21:26:52 -0500

    Define namespace GRAPH

--- src/scnnoise/graph.cpp
+++ src/scnnoise/graph.cpp
@@ -4,160 +4,162 @@
 #include <iostream>
 #include "graph.hpp"
 
-// Constructor
-Graph::Graph (int N) {
-  // const int sz = 1000
-  adj_list.reserve(N);
-  parent_list.reserve(N);
-  num_nodes = N;
-  // adj_list.resize(N);
-}
+namespace GRAPH {
+  // Constructor
+  Graph::Graph (int N) {
+    // const int sz = 1000
+    adj_list.reserve(N);
+    parent_list.reserve(N);
+    num_nodes = N;
+    // adj_list.resize(N);
+  }
 
-int Graph::get_size () {
-  return num_nodes;
-}
+  int Graph::get_size () {
+    return num_nodes;
+  }
 
-// // Function to add edge
-// inline void Graph::add_edge (int src, int dest, double max_expr, double hill_coeff,
-//   double half_maximal) {
-//   adj_list[src].push_back(dest);
-//   parent_list[dest].push_back(src);
-//   edge_kinetic_params[src][dest].max_expr = max_expr;
-//   edge_kinetic_params[src][dest].hill_coeff = hill_coeff;
-//   edge_kinetic_params[src][dest].half_maximal = half_maximal;
-// }
+  // // Function to add edge
+  // inline void Graph::add_edge (int src, int dest, double max_expr, double hill_coeff,
+  //   double half_maximal) {
+  //   adj_list[src].push_back(dest);
+  //   parent_list[dest].push_back(src);
+  //   edge_kinetic_params[src][dest].max_expr = max_expr;
+  //   edge_kinetic_params[src][dest].hill_coeff = hill_coeff;
+  //   edge_kinetic_params[src][dest].half_maximal = half_maximal;
+  // }
 
-// Function to delete edge
-void Graph::del_edge (int src, int dest) {
-  adj_list[src].erase(std::remove(adj_list[src].begin(),
-  adj_list[src].end(), dest), adj_list[src].end());
-}
+  // Function to delete edge
+  void Graph::del_edge (int src, int dest) {
+    adj_list[src].erase(std::remove(adj_list[src].begin(),
+    adj_list[src].end(), dest), adj_list[src].end());
+  }
 
-// Make graph undirected.
-void Graph::make_undirected (std::vector<Edge> &edges) {
-  int count = 0;
-  for(int i = 0; i < num_nodes; ++i){
-    for (int v: adj_list[i]){
-      std::vector<int>::iterator it;
-      it = find(adj_list[v].begin(), adj_list[v].end(), i);
-      if (it != adj_list[v].end()) {
-        Edge edge;
-        edge.src = v;
-        edge.dest = i;
-        edges.push_back(edge);
+  // Make graph undirected.
+  void Graph::make_undirected (std::vector<Edge> &edges) {
+    int count = 0;
+    for(int i = 0; i < num_nodes; ++i){
+      for (int v: adj_list[i]){
+        std::vector<int>::iterator it;
+        it = find(adj_list[v].begin(), adj_list[v].end(), i);
+        if (it != adj_list[v].end()) {
+          Edge edge;
+          edge.src = v;
+          edge.dest = i;
+          edges.push_back(edge);
+        }
+        else {
+          Edge edge;
+          edge.src = -1;
+          edge.dest = -1;
+          edges.push_back(edge);
+        }
+        count += 1;
       }
-      else {
-        Edge edge;
-        edge.src = -1;
-        edge.dest = -1;
-        edges.push_back(edge);
-      }
-      count += 1;
+
     }
 
-  }
-
-  for (auto edge: edges) {
-    if (edge.src != -1) {
-      adj_list[edge.src].push_back(edge.dest);
-    }
-  }
-}
-
-// Unmake graph undirected.
-void Graph::unmake_undirected (std::vector<Edge> &edges) {
-  for (auto edge: edges) {
-    if (edge.src != -1) {
-      adj_list[edge.src].push_back(edge.dest);
-      adj_list[edge.src].erase(std::remove(adj_list[edge.src].begin(),
-      adj_list[edge.src].end(), edge.dest), adj_list[edge.src].end());
-    }
-  }
-}
-
-// Recursive DFS function for checking graph connectedness.
-void Graph::is_connected_DFS (int vert, std::vector<bool> visited) {
-  if (!visited[vert]) {
-    std::cout << "current node = " << vert << std::endl;
-    visited[vert] = true;
-    for (int v: adj_list[vert]) {
-      if(!visited[v]){
-        is_connected_DFS(v, visited);
+    for (auto edge: edges) {
+      if (edge.src != -1) {
+        adj_list[edge.src].push_back(edge.dest);
       }
     }
   }
-}
 
-// Function to check whether graph is connected or not.
-bool Graph::is_connected () {
-  std::vector<bool> visited(num_nodes);
-  std::vector<Edge> edges;
-  for (auto vert: visited) {
-    vert = false;
-  }
-  make_undirected(edges);
-  is_connected_DFS(0, visited);
-  unmake_undirected(edges);
-  edges.clear();
-  int count = 0;
-  for(int i = 0; i < num_nodes; ++i){
-    if(visited[i]){
-      count += 1;
-    }
-  }
-  if(count == num_nodes){
-    return true;
-  }else{
-    return false;
-  }
-}
-
-// Recursive DFS utility function for determining whether graph is DAG or not.
-bool Graph::is_DAG_util (int vert, std::vector<bool> visited,
-                         std::vector<bool> active) {
-
-  visited[vert] = true;
-  active[vert] = true;
-  for (int v: adj_list[vert]) {
-    if(!visited[v]){
-      if(!is_DAG_util(v, visited, active)){
-        return false;
+  // Unmake graph undirected.
+  void Graph::unmake_undirected (std::vector<Edge> &edges) {
+    for (auto edge: edges) {
+      if (edge.src != -1) {
+        adj_list[edge.src].push_back(edge.dest);
+        adj_list[edge.src].erase(std::remove(adj_list[edge.src].begin(),
+        adj_list[edge.src].end(), edge.dest), adj_list[edge.src].end());
       }
     }
-    else if(active[v]){
+  }
+
+  // Recursive DFS function for checking graph connectedness.
+  void Graph::is_connected_DFS (int vert, std::vector<bool> visited) {
+    if (!visited[vert]) {
+      std::cout << "current node = " << vert << std::endl;
+      visited[vert] = true;
+      for (int v: adj_list[vert]) {
+        if(!visited[v]){
+          is_connected_DFS(v, visited);
+        }
+      }
+    }
+  }
+
+  // Function to check whether graph is connected or not.
+  bool Graph::is_connected () {
+    std::vector<bool> visited(num_nodes);
+    std::vector<Edge> edges;
+    for (auto vert: visited) {
+      vert = false;
+    }
+    make_undirected(edges);
+    is_connected_DFS(0, visited);
+    unmake_undirected(edges);
+    edges.clear();
+    int count = 0;
+    for(int i = 0; i < num_nodes; ++i){
+      if(visited[i]){
+        count += 1;
+      }
+    }
+    if(count == num_nodes){
+      return true;
+    }else{
       return false;
     }
-    else{}
   }
-  active[vert] = false;
-  return true;
-}
 
-// Function to check whether graph is DAG or not.
-bool Graph::is_DAG () {
-  std::vector<bool> visited(num_nodes);
-  std::vector<bool> active(num_nodes);
-  for (auto vert: visited) {
-    vert = false;
-  }
-  for (auto vert: active) {
-    vert = false;
-  }
-  for(int v = 0; v < num_nodes; ++v){
-    if(!visited[v]){
-      if(!is_DAG_util(v, visited, active)){
+  // Recursive DFS utility function for determining whether graph is DAG or not.
+  bool Graph::is_DAG_util (int vert, std::vector<bool> visited,
+                           std::vector<bool> active) {
+
+    visited[vert] = true;
+    active[vert] = true;
+    for (int v: adj_list[vert]) {
+      if(!visited[v]){
+        if(!is_DAG_util(v, visited, active)){
+          return false;
+        }
+      }
+      else if(active[v]){
         return false;
       }
+      else{}
     }
+    active[vert] = false;
+    return true;
   }
-  return true;
-}
 
-// Function to return children nodes of a given node.
-void Graph::find_children (int vert, std::vector<int> &children) {
-  if (!adj_list[vert].empty()) {
-    for (int v: adj_list[vert]) {
-      children.push_back(v);
+  // Function to check whether graph is DAG or not.
+  bool Graph::is_DAG () {
+    std::vector<bool> visited(num_nodes);
+    std::vector<bool> active(num_nodes);
+    for (auto vert: visited) {
+      vert = false;
+    }
+    for (auto vert: active) {
+      vert = false;
+    }
+    for(int v = 0; v < num_nodes; ++v){
+      if(!visited[v]){
+        if(!is_DAG_util(v, visited, active)){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  // Function to return children nodes of a given node.
+  void Graph::find_children (int vert, std::vector<int> &children) {
+    if (!adj_list[vert].empty()) {
+      for (int v: adj_list[vert]) {
+        children.push_back(v);
+      }
     }
   }
 }
--- src/scnnoise/graph.hpp
+++ src/scnnoise/graph.hpp
@@ -9,106 +9,108 @@ struct Edge {
   int dest;
 };
 
-class Graph {
-protected:
-  /* Data */
-  /********************************************//**
-   \brief Number of nodes in the graph.
-   ***********************************************/
-  int num_nodes;
+namespace GRAPH {
+  class Graph {
+  protected:
+    /* Data */
+    /********************************************//**
+     \brief Number of nodes in the graph.
+     ***********************************************/
+    int num_nodes;
 
-  /********************************************//**
-   \brief A vector of vectors to represent adjacency list.
-   ***********************************************/
-  std::vector<std::vector<int>> adj_list;
-  std::vector<std::vector<int>> parent_list;
-  // std::vector<std::map<int, edge_struct>> edge_kinetic_params;
+    /********************************************//**
+     \brief A vector of vectors to represent adjacency list.
+     ***********************************************/
+    std::vector<std::vector<int>> adj_list;
+    std::vector<std::vector<int>> parent_list;
+    // std::vector<std::map<int, edge_struct>> edge_kinetic_params;
 
-  /* Memeber functions */
-  /********************************************//**
-   \brief Recursive DFS function for checking graph connectedness.
+    /* Memeber functions */
+    /********************************************//**
+     \brief Recursive DFS function for checking graph connectedness.
 
-   \param[in] vert vertex in the graph from where the DFS is initiated.
-   \param[in] visited boolean vector to track whether vertices in the
-              graph have been visited by the DFS or not.
-   ***********************************************/
-  void is_connected_DFS (int vert, std::vector<bool> visited);
+     \param[in] vert vertex in the graph from where the DFS is initiated.
+     \param[in] visited boolean vector to track whether vertices in the
+                graph have been visited by the DFS or not.
+     ***********************************************/
+    void is_connected_DFS (int vert, std::vector<bool> visited);
 
-  // Make graph undirected.
-  /********************************************//**
-   \brief Make graph undirected for checking weak connectedness.
+    // Make graph undirected.
+    /********************************************//**
+     \brief Make graph undirected for checking weak connectedness.
 
-   \param[in] vector of edges to store edges added to make the graph
-              undirected.
-   ***********************************************/
-  void make_undirected (std::vector<Edge> &edges);
+     \param[in] vector of edges to store edges added to make the graph
+                undirected.
+     ***********************************************/
+    void make_undirected (std::vector<Edge> &edges);
 
-  /********************************************//**
-   \brief Unmake graph undirected after checking for weak connectedness.
+    /********************************************//**
+     \brief Unmake graph undirected after checking for weak connectedness.
 
-   \param[in] vector of edges to store edges to delete
-              to make the graph directed again.
-   ***********************************************/
-  void unmake_undirected (std::vector<Edge> &edges);
+     \param[in] vector of edges to store edges to delete
+                to make the graph directed again.
+     ***********************************************/
+    void unmake_undirected (std::vector<Edge> &edges);
 
-  // Recursive DFS utility function for determining whether graph is DAG or not.
-  /********************************************//**
-   \brief Recursive DFS utility function for determining whether
-          graph is DAG or not.
+    // Recursive DFS utility function for determining whether graph is DAG or not.
+    /********************************************//**
+     \brief Recursive DFS utility function for determining whether
+            graph is DAG or not.
 
-   \param[in] vert vertex in the graph from where the DFS is initiated.
-   \param[in] visited boolean vector to track whether vertices in the
-              graph have been visited by the DFS or not.
-   ***********************************************/
-  bool is_DAG_util (int vert, std::vector<bool> visited,
-                    std::vector<bool> active);
+     \param[in] vert vertex in the graph from where the DFS is initiated.
+     \param[in] visited boolean vector to track whether vertices in the
+                graph have been visited by the DFS or not.
+     ***********************************************/
+    bool is_DAG_util (int vert, std::vector<bool> visited,
+                      std::vector<bool> active);
 
 
 
-public:
-  /* Memeber functions */
-  /********************************************//**
-   \brief Graph Constructor.
+  public:
+    /* Memeber functions */
+    /********************************************//**
+     \brief Graph Constructor.
 
-   \param[in] N Number of nodes in the graph.
-   ***********************************************/
-  Graph (int N);
+     \param[in] N Number of nodes in the graph.
+     ***********************************************/
+    Graph (int N);
 
-  int get_size ();
+    int get_size ();
 
-  /********************************************//**
-   \brief Function to add edge to the graph.
+    /********************************************//**
+     \brief Function to add edge to the graph.
 
-   \param[in] src source vertex for the edge.
-   \param[in] dest destination vertex for the edge.
-   ***********************************************/
-  virtual void add_edge (int src, int dest) = 0;
+     \param[in] src source vertex for the edge.
+     \param[in] dest destination vertex for the edge.
+     ***********************************************/
+    virtual void add_edge (int src, int dest) = 0;
 
-  /********************************************//**
-   \brief Function to delete edge from the graph.
+    /********************************************//**
+     \brief Function to delete edge from the graph.
 
-   \param[in] src source vertex for the edge.
-   \param[in] dest destination vertex for the edge.
-   ***********************************************/
-  void del_edge (int src, int dest);
+     \param[in] src source vertex for the edge.
+     \param[in] dest destination vertex for the edge.
+     ***********************************************/
+    void del_edge (int src, int dest);
 
-  /********************************************//**
-   \brief Function to check whether graph is weakly connected or not.
-   ***********************************************/
-  bool is_connected ();
+    /********************************************//**
+     \brief Function to check whether graph is weakly connected or not.
+     ***********************************************/
+    bool is_connected ();
 
-  /********************************************//**
-   \brief Function to check whether graph is DAG or not.
-   ***********************************************/
-  bool is_DAG ();
+    /********************************************//**
+     \brief Function to check whether graph is DAG or not.
+     ***********************************************/
+    bool is_DAG ();
 
-  /********************************************//**
-   \brief Function to return children nodes of a given node.
+    /********************************************//**
+     \brief Function to return children nodes of a given node.
 
-   \param[in] vert vertex to return children for.
-   \param[in] children vector to store the children nodes.
-   ***********************************************/
-  void find_children (int vert, std::vector<int> &children);
-};
+     \param[in] vert vertex to return children for.
+     \param[in] children vector to store the children nodes.
+     ***********************************************/
+    void find_children (int vert, std::vector<int> &children);
+  };
+}
 
 #endif

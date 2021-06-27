fcf8a638c08f5bd78b96edb10706a4c067d7935f
Tarun-Mahajan <tiit828@gmail.com>
2021-06-27 17:02:00 -0500

    Remove namespaces SimulatorGillespieSSA and SimulatorGillespieSDM

--- src/scnnoise/gillespieSDM.cpp
+++ src/scnnoise/gillespieSDM.cpp
@@ -12,27 +12,23 @@
 
 
 namespace ScnnoiseInterface {
-  namespace SimulatorGillespieSSA {
-    namespace SimulatorGillespieSDM {
-      /* function definitions */
-      // Constructor
-      GillespieSDM::GillespieSDM (int num_rxns, int num_genes,
-                                  const std::vector<int> num_species_gene_type,
-                                  const std::vector<int> num_rxns_gene_type,
-                                  double max_time,
-                                  bool save_timeseries, int num_timepoints_save,
-                                  std::string count_save_file):
-        GillespieSSA (num_rxns, num_genes, num_species_gene_type,
-          num_rxns_gene_type, max_time, save_timeseries, num_timepoints_save,
-          count_save_file) {
-      }
+  /* function definitions */
+  // Constructor
+  GillespieSDM::GillespieSDM (int num_rxns, int num_genes,
+                              const std::vector<int> num_species_gene_type,
+                              const std::vector<int> num_rxns_gene_type,
+                              double max_time,
+                              bool save_timeseries, int num_timepoints_save,
+                              std::string count_save_file):
+    GillespieSSA (num_rxns, num_genes, num_species_gene_type,
+      num_rxns_gene_type, max_time, save_timeseries, num_timepoints_save,
+      count_save_file) {
+  }
 
-      void GillespieSDM::sort_reaction (int &rxn_selected) override {
-        if (rxn_selected > 0) {
-          std::swap(rxn_order[rxn_selected - 1], rxn_order[rxn_selected]);
-          rxn_selected -= 1;
-        }
-      }
+  void GillespieSDM::sort_reaction (int &rxn_selected) override {
+    if (rxn_selected > 0) {
+      std::swap(rxn_order[rxn_selected - 1], rxn_order[rxn_selected]);
+      rxn_selected -= 1;
     }
   }
 }
--- src/scnnoise/gillespieSDM.hpp
+++ src/scnnoise/gillespieSDM.hpp
@@ -3,36 +3,32 @@
 #define SDM_H
 
 namespace ScnnoiseInterface {
-  namespace SimulatorGillespieSSA {
-    namespace SimulatorGillespieSDM {
-      /********************************************//**
-       \brief A class for Gillespie's stochastic simulation algorithm.
+  /********************************************//**
+   \brief A class for Gillespie's stochastic simulation algorithm.
 
-       The Gillespie class creates an object to perform
-       exact stochastic simulation for any given chemical
-       reaction network.
-       ***********************************************/
+   The Gillespie class creates an object to perform
+   exact stochastic simulation for any given chemical
+   reaction network.
+   ***********************************************/
 
-      class GillespieSDM: public GillespieSSA {
-      private:
+  class GillespieSDM: public GillespieSSA {
+  private:
 
-      public:
-        typedef std::mt19937 RNG;
-        /* Member functions */
-        // Constructor
-        GillespieSDM (int num_rxns, int num_genes,
-          const std::vector<int> num_species_gene_type,
-          const std::vector<int> num_rxns_gene_type, double max_time,
-          bool save_timeseries, int num_timepoints_save,
-          std::string count_save_file);
+  public:
+    typedef std::mt19937 RNG;
+    /* Member functions */
+    // Constructor
+    GillespieSDM (int num_rxns, int num_genes,
+      const std::vector<int> num_species_gene_type,
+      const std::vector<int> num_rxns_gene_type, double max_time,
+      bool save_timeseries, int num_timepoints_save,
+      std::string count_save_file);
 
-        // Virtual destructor
-        virtual ~GillespieSDM ();
+    // Virtual destructor
+    virtual ~GillespieSDM ();
 
-        void sort_reaction (int &rxn_selected) override;
-      };
-    }
-  }
+    void sort_reaction (int &rxn_selected) override;
+  };
 }
 
 #endif

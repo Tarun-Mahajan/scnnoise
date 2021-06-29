3ef8e1c64dfaeab2351858c6b4cb1e490d18e258
Tarun-Mahajan <tiit828@gmail.com>
2021-06-28 21:37:30 -0500

    Add edge_rxn_params to parent_list and not adj_list

--- src/scnnoise/GRN.cpp
+++ src/scnnoise/GRN.cpp
@@ -18,14 +18,14 @@ namespace GraphSpace {
   void GRN::add_edge_kinetics (int src, int dest, double prob_contr,
                                double hill_coeff, double half_maximal,
                                int rxn_IN, int species_OUT) {
-                               add_edge(src, dest);
-                               edge_rxn_struct edge_rxn_temp;
-                               edge_rxn_temp.prob_contr = prob_contr;
-                               edge_rxn_temp.hill_coeff = hill_coeff;
-                               edge_rxn_temp.half_maximal = half_maximal;
-                               edge_rxn_temp.rxn_IN = rxn_IN;
-                               edge_rxn_temp.species_OUT = species_OUT;
-                               edge_rxn_params[src].push_back(edge_rxn_temp);
+                              add_edge(src, dest);
+                              edge_rxn_struct edge_rxn_temp;
+                              edge_rxn_temp.prob_contr = prob_contr;
+                              edge_rxn_temp.hill_coeff = hill_coeff;
+                              edge_rxn_temp.half_maximal = half_maximal;
+                              edge_rxn_temp.rxn_IN = rxn_IN;
+                              edge_rxn_temp.species_OUT = species_OUT;
+                              edge_rxn_params[dest].push_back(edge_rxn_temp);
                              }
   inline void GRN::add_edge (int src, int dest) {
     adj_list[src].push_back(dest);

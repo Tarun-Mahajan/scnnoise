b5d1106c32c32bc5071aa6dc9d464283e221f2f9
Tarun-Mahajan <tiit828@gmail.com>
2021-07-01 18:17:21 -0500

    Add more tests to check if class object properly initialized

--- tests/test_scnnoise.cpp
+++ tests/test_scnnoise.cpp
@@ -18,4 +18,11 @@ TEST_CASE("Test scNNoiSE", )
                                        max_time, save_timeseries,
                                        num_timepoints_save, count_save_file);
     REQUIRE(obj.num_rxns == num_rxns);
+    REQUIRE(obj.num_genes == num_genes);
+    REQUIRE(obj.num_species_gene_type == num_species_gene_type);
+    REQUIRE(obj.num_rxns_gene_type == num_rxns_gene_type);
+    REQUIRE(obj.max_time == max_time);
+    REQUIRE(obj.save_timeseries == save_timeseries);
+    REQUIRE(obj.num_timepoints_save == num_timepoints_save);
+    REQUIRE(obj.count_save_file == count_save_file);
 }

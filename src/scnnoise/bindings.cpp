3752f430381b0c01186dc3527d6c7a68bfb92829
Tarun-Mahajan <tiit828@gmail.com>
2021-07-02 03:22:10 -0500

    Remove test classes

--- src/scnnoise/bindings.cpp
+++ src/scnnoise/bindings.cpp
@@ -6,7 +6,6 @@
 #include "scnnoise.hpp"
 #include "gillespieSSA.hpp"
 #include "gillespieSDM.hpp"
-#include "class12.hpp"
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
@@ -26,15 +25,6 @@ PYBIND11_MODULE(scnnoise, m) {
               ScnnoiseInterface::GillespieSSA>(m, "GillespieSDM")
         .def(py::init<int, int, std::vector<int>, std::vector<int>,
              double, bool, int, std::string>());
-// For testing the binding
-    py::class_<CLASS_::class1>(m, "class1")
-         // .def(py::init<int, int>());
-        .def("add", &CLASS_::class1::add);
-         // .def("print_address", &CLASS_::class1::print_address);
-         // .def("subtract_", &class1::subtract_);
-    py::class_<CLASS_::class2, CLASS_::class1>(m, "class2")
-        .def(py::init<py::list>())
-        .def("subtract_", &CLASS_::class2::subtract_);
 
 #ifdef VERSION_INFO
  m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);

1e12cd6c20dd3a7fc73e01813264509c3eee2e1e
Tarun-Mahajan <tiit828@gmail.com>
2021-07-02 00:09:42 -0500

    Remove const from vector argument

--- tests/test_scnnoise.cpp
+++ tests/test_scnnoise.cpp
@@ -7,8 +7,8 @@ TEST_CASE("Test scNNoiSE")
 {
     int num_rxns = 14;
     int num_genes = 2;
-    const std::vector<int> num_species_gene_type(1, 5);
-    const std::vector<int> num_rxns_gene_type(1, 7);
+    std::vector<int> num_species_gene_type(1, 5);
+    std::vector<int> num_rxns_gene_type(1, 7);
     double max_time = 10000;
     bool save_timeseries = true;
     int num_timepoints_save = 100;

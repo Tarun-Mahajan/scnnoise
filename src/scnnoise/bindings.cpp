9c63f72e39c77fe6114068fd088fe1d66d442b5c
Tarun-Mahajan <tiit828@gmail.com>
2021-07-04 02:01:20 -0500

    Include 'stl.h' from pybind11

--- src/scnnoise/bindings.cpp
+++ src/scnnoise/bindings.cpp
@@ -1,5 +1,5 @@
 #include <pybind11/pybind11.h>
-#include <pybind11/stl_bind.h>
+#include <pybind11/stl.h>
 // #include "../../lib/pybind11/pybind11/include/pybind11/pybind11.h"
 // #include "../../lib/pybind11/pybind11/include/pybind11/stl.h"
 #include <string>
@@ -13,7 +13,7 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(_scnnoise, m) {
-    py::bind_vector<std::vector<int>>(m, "VectorInt", py::buffer_protocol());
+    // py::bind_vector<std::vector<int>>(m, "VectorInt", py::buffer_protocol());
     py::class_<ScnnoiseInterface::scNNoiSE>(m, "scNNoiSE")
         .def("add_gene_state", &ScnnoiseInterface::scNNoiSE::add_gene_state)
         .def("add_GRN_edge", &ScnnoiseInterface::scNNoiSE::add_GRN_edge)

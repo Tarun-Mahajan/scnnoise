dcdd60bd2d6cc1f28ca0ae877c5cadc65ddcde94
Tarun-Mahajan <tiit828@gmail.com>
2021-07-04 02:18:36 -0500

    Add section to test constructor for class 'gillespieSDMnoCellCycle'

--- tests/test_scnnoise.cpp
+++ tests/test_scnnoise.cpp
@@ -17,12 +17,14 @@ TEST_CASE("Test scNNoiSE")
                                        num_species_gene_type, num_rxns_gene_type,
                                        max_time, save_timeseries,
                                        num_timepoints_save, count_save_file);
-    REQUIRE(num_rxns == num_rxns);
-    REQUIRE(obj.num_genes == num_genes);
-    REQUIRE(obj.num_species_gene_type == num_species_gene_type);
-    REQUIRE(obj.num_rxns_gene_type == num_rxns_gene_type);
-    REQUIRE(obj.max_time == max_time);
-    REQUIRE(obj.save_timeseries == save_timeseries);
-    REQUIRE(obj.num_timepoints_save == num_timepoints_save);
-    REQUIRE(obj.count_save_file == count_save_file);
+    section ('Test constructor') {
+        REQUIRE(num_rxns == num_rxns);
+        REQUIRE(obj.num_genes == num_genes);
+        REQUIRE(obj.num_species_gene_type == num_species_gene_type);
+        REQUIRE(obj.num_rxns_gene_type == num_rxns_gene_type);
+        REQUIRE(obj.max_time == max_time);
+        REQUIRE(obj.save_timeseries == save_timeseries);
+        REQUIRE(obj.num_timepoints_save == num_timepoints_save);
+        REQUIRE(obj.count_save_file == count_save_file);
+    }
 }

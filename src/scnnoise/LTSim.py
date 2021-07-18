41de75d0d65acd6cdce6d529f92a870b2339a637
Luka Karginov <lukakarginov@Lukas-MacBook-Pro.local>
2021-07-17 19:49:00 -0500

    adding jupyter notebook for testing and basic docstring

--- src/scnnoise/LTSim.py
+++ src/scnnoise/LTSim.py
@@ -1,5 +1,9 @@
 class LTSim:
     def __init__(self, root, num_samples)
+        """
+        Constructor for the LTSim object
+        Takes in the root of the LineageTree and the number of samples in the simulation
+        """
         self.root = root    #cell Type
         self.num_samples = num_samples     #number of samples
 

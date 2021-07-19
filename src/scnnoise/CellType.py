536f5d2e7bc4e5ee41dd7b5dd677cfb26353ad77
Luka Karginov <lukakarginov@Lukas-MacBook-Pro.local>
2021-07-19 15:44:27 -0500

    commented basic plan into functions and added simple recursive code

--- src/scnnoise/CellType.py
+++ src/scnnoise/CellType.py
@@ -17,6 +17,20 @@ class cellType:
         Simulates num_samples reads of cellType 
         and outputs num_samples x genes Matrix of raw mRNA counts
         """
+        #Recursive function (careful with passing by reference/by copy)
+        #Step 1: simulate gene expression to steady state
+        #Step 2: Sample num_samples reads for each gene
+        #Step 3: Recusively run sim_transition() and sim_cell_type() on all children (this could be parallelized)
+        
+        #base case
+        if len(children) == 0:
+            return 
+        
+        #recursive case
+        else:
+            for cell_type in self.children:
+                cell_type.sim_transition(num_samples, simulator)
+                cell_type.sim_cell_type(num_samples, simulator)
         
     def sim_transition(num_samples, simulator):
         """
@@ -27,4 +41,10 @@ class cellType:
         Simulates num_samples reads of cells transitioning from the parent cell type 
         and outputs num_samples x genes Matrix of raw mRNA counts
         """
+        #helper (careful with passing by reference/by copy)
+        #Step 1: simluate transition from parent steady state to daughter steady state
+            #a. here we check when the system reaches a new steady state and sample before then
+            #b. if steady states are similar mbe set simulation time?
+        #Step 2: Sample num_sample transition cells and store to output
+        
         
--- src/scnnoise/LTSim.py
+++ src/scnnoise/LTSim.py
@@ -11,4 +11,12 @@ class LTSim:
         """
         Outputs a n x g Matrix (n cells by g Genes) of raw mRNA counts
         """
+        #Step 1: Construct simulator object
+        #a. build GRN from params
+        #b. define intragene interactions
+        
+        #Step 2: Run sim_cell_type() on root
+        
+        #Step 3: return result file, or data structure
+        
         return self.sample

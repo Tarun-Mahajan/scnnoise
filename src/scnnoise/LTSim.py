88e0488535704d8f6d6917f7fe407a044ff29654
Luka Karginov <lukakarginov@Lukas-MacBook-Pro.local>
2021-07-20 18:46:19 -0500

    added additional strucutre

--- src/scnnoise/LTSim.py
+++ src/scnnoise/LTSim.py
@@ -1,13 +1,12 @@
 class LTSim:
-    def __init__(self, root, num_samples):
+    def __init__(self, root):
         """
         Constructor for the LTSim object
-        Takes in the root of the LineageTree and the number of samples in the simulation
+        Takes in the root of the LineageTree in the simulation
         """
         self.root = root    #cell Type
-        self.num_samples = num_samples     #number of samples
 
-    def sim_LT():
+    def sim_LT(num_samples):
         """
         Outputs a n x g Matrix (n cells by g Genes) of raw mRNA counts
         """
@@ -16,7 +15,8 @@ class LTSim:
         #b. define intragene interactions
         
         #Step 2: Run sim_cell_type() on root
+        self.root.sim_cell_type(num_samples, simulator)
         
         #Step 3: return result file, or data structure
-        
+            
         return self.sample

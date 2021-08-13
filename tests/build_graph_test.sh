999861ea2cfed994fbcddd67a7465e2bf70651f3
Tarun-Mahajan <tiit828@gmail.com>
2021-08-13 13:40:44 -0500

    Add input parameters to provide to scnnoise_graph_test at runtime

--- tests/build_graph_test.sh
+++ tests/build_graph_test.sh
@@ -2,4 +2,7 @@
 cd ../build
 cmake ../tests
 cmake --build . --target scnnoise_graph_test
-./scnnoise_graph_test
+
+GRN_filepath="../data/test_GRN/GRN_size_6_net_1.csv"
+num_genes=6
+./scnnoise_graph_test -g $GRN_filepath --num_nodes $num_genes

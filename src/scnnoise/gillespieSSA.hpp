3ee58a9549443c599ddd2e704ab9cab39454e768
Tarun-Mahajan <tiit828@gmail.com>
2021-08-24 00:02:17 -0500

    update_fired_reaction returns a vector of strings

--- src/scnnoise/gillespieSSA.hpp
+++ src/scnnoise/gillespieSSA.hpp
@@ -34,7 +34,7 @@ namespace ScnnoiseInterface {
     int sample_next_rxn (RNG &generator);
 
     // Update molecule counts by firing the selected reaction
-    std::vector<bool> update_fired_reaction (int rxn_selected);
+    std::vector<std::string> update_fired_reaction (int rxn_selected);
 
     // Update propensity for reactions dependent via the GRN
     void update_dependent_count_propensity (int rxn_selected,

60af2e6f6a30d211aebfd46f5b90649ffed40c8a
Tarun-Mahajan <tiit828@gmail.com>
2021-08-24 23:10:23 -0500

    Fix indentation

--- src/scnnoise/gillespieSDMnoCellCycle.cpp
+++ src/scnnoise/gillespieSDMnoCellCycle.cpp
@@ -11,31 +11,31 @@
 
 
 namespace ScnnoiseInterface {
-  /* function definitions */
-  // Constructor
-  gillespieSDMnoCellCycle::gillespieSDMnoCellCycle (int num_genes, std::string gene_filepath,
-      std::string GRN_filepath, std::string molecule_count_filepath,
-      std::string count_save_file):
+    /* function definitions */
+    // Constructor
+    gillespieSDMnoCellCycle::gillespieSDMnoCellCycle (int num_genes, std::string gene_filepath,
+        std::string GRN_filepath, std::string molecule_count_filepath,
+        std::string count_save_file):
     GillespieSSA (num_genes, gene_filepath,
         GRN_filepath, molecule_count_filepath, count_save_file) {
-  }
-
-  void gillespieSDMnoCellCycle::sort_reaction (int &rxn_selected) {
-    if (rxn_selected > 0) {
-        std::string gene_name = gene_map[rxn_order[rxn_selected].gene_id];
-        std::string rxn_name = rxn_order[rxn_selected].rxn_name;
-        rxn_order_map[gene_name][rxn_name] -= 1;
-
-        gene_name = gene_map[rxn_order[rxn_selected - 1].gene_id];
-        rxn_name = rxn_order[rxn_selected - 1].rxn_name;
-        rxn_order_map[gene_name][rxn_name] += 1;
-
-        std::swap(rxn_order[rxn_selected - 1], rxn_order[rxn_selected]);
-        rxn_selected -= 1;
     }
-  }
 
-  inline void gillespieSDMnoCellCycle::update_cell_cycle_state (double cur_time) {
+    void gillespieSDMnoCellCycle::sort_reaction (int &rxn_selected) {
+        if (rxn_selected > 0) {
+            std::string gene_name = gene_map[rxn_order[rxn_selected].gene_id];
+            std::string rxn_name = rxn_order[rxn_selected].rxn_name;
+            rxn_order_map[gene_name][rxn_name] -= 1;
 
-  }
+            gene_name = gene_map[rxn_order[rxn_selected - 1].gene_id];
+            rxn_name = rxn_order[rxn_selected - 1].rxn_name;
+            rxn_order_map[gene_name][rxn_name] += 1;
+
+            std::swap(rxn_order[rxn_selected - 1], rxn_order[rxn_selected]);
+            rxn_selected -= 1;
+        }
+    }
+
+    inline void gillespieSDMnoCellCycle::update_cell_cycle_state (double cur_time) {
+
+    }
 }
--- src/scnnoise/gillespieSDMnoCellCycle.hpp
+++ src/scnnoise/gillespieSDMnoCellCycle.hpp
@@ -7,27 +7,27 @@
 #include "gillespieSSA.hpp"
 
 namespace ScnnoiseInterface {
-  /********************************************//**
-   \brief A class for Gillespie's stochastic simulation algorithm.
+    /********************************************//**
+    \brief A class for Gillespie's stochastic simulation algorithm.
 
-   The Gillespie class creates an object to perform
-   exact stochastic simulation for any given chemical
-   reaction network.
-   ***********************************************/
+    The Gillespie class creates an object to perform
+    exact stochastic simulation for any given chemical
+    reaction network.
+    ***********************************************/
 
-  class gillespieSDMnoCellCycle : public GillespieSSA {
-  private:
+    class gillespieSDMnoCellCycle : public GillespieSSA {
+    private:
 
-  public:
+    public:
     /* Member functions */
     // Constructor
-    gillespieSDMnoCellCycle (int num_genes, std::string gene_filepath,
-        std::string GRN_filepath, std::string molecule_count_filepath,
-        std::string count_save_file);
+        gillespieSDMnoCellCycle (int num_genes, std::string gene_filepath,
+            std::string GRN_filepath, std::string molecule_count_filepath,
+            std::string count_save_file);
 
-    void sort_reaction (int &rxn_selected) override;
-    void update_cell_cycle_state (double cur_time) override;
-  };
+        void sort_reaction (int &rxn_selected) override;
+        void update_cell_cycle_state (double cur_time) override;
+    };
 }
 
 #endif

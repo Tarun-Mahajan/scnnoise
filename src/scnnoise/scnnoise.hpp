585b031f0531b4799390f382210fc5c28b9a8a67
Tarun-Mahajan <tiit828@gmail.com>
2021-09-21 20:07:05 -0500

    Add argument 'compute_statistics' to func. 'simulate' to decide whether to store statistics at the end of simulation

--- src/scnnoise/scnnoise.hpp
+++ src/scnnoise/scnnoise.hpp
@@ -595,7 +595,7 @@ namespace ScnnoiseInterface {
         expression. Needs to be overridden in any derived class.
         ***********************************************/
         virtual void simulate (RNG &generator, double num_repeat, bool verbose,
-            std::string statistics_file) = 0;
+            bool compute_statistics = true, std::string statistics_file) = 0;
 
         void set_simulation_params (double max_time = 10000,
             bool save_timeseries = false);

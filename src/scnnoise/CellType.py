0852605e2e83b42f2ac552e15dde3300b9c58768
Luka Karginov <lukakarginov@Lukas-MacBook-Pro.local>
2021-09-25 14:26:13 -0500

    removing count_csv/sample_csv from constructor and adding to recursive methods

--- src/scnnoise/CellType.py
+++ src/scnnoise/CellType.py
@@ -4,22 +4,20 @@ import pandas as pd
 import numpy as np
 
 class CellType:
-    def __init__(self, lineageName,rxn_rate_csv, children, count_csv, sample_csv):
+    def __init__(self, lineageName,rxn_rate_csv, children):
         """
         Takes in rxn_rates and children and constructs a cellType node
         """
         #get rate parameters from external file or otherwise
         self.lineageName = lineageName
         self.children = children
-        self.count_csv = count_csv
-        self.sample_csv = sample_csv
         cell_types = pd.read_csv(rxn_rate_csv)
         curr_cell_type = cell_types[cell_types['Cell Type'] == self.lineageName]
         curr_cell_type.index = curr_cell_type['Gene']
         curr_cell_type = curr_cell_type.drop(columns = ['Cell Type', 'Gene'])
         self.rxn_rates = curr_cell_type.T.to_dict()
 
-    def sim_cell_type(self, num_samples, simulator, molecule):
+    def sim_cell_type(self, num_samples, simulator, molecule, count_csv, sample_csv):
         """
         Params
             num_samples - number of sample reads to output for this cell type
@@ -32,7 +30,7 @@ class CellType:
         #Step 1: simulate gene expression at steady state
             #a. it would be nice to be able to pass the 4 to simulate for into the simulate function
         print(str(self.lineageName))
-        f = open(self.count_csv, "w")
+        f = open(count_csv, "w")
         f.truncate()
         f.close()
         simulator.set_simulation_params(1000, True)
@@ -41,13 +39,13 @@ class CellType:
             #for steady state check sergio method
         
         #Step 2: Sample num_samples reads for each gene
-        sim_out = pd.read_csv(self.count_csv)
+        sim_out = pd.read_csv(count_csv)
         sim_out = sim_out.loc[:, ~sim_out.columns.str.contains('^Unnamed')]
         species = [col for col in sim_out.columns if col[-len(molecule):] == molecule]
         samples = np.random.randint(0,len(sim_out.index), size = num_samples)
         sample_out = sim_out.iloc[samples][species]
         sample_out['Cell Type'] = [str(self.lineageName)] * num_samples
-        sample_out.to_csv(self.sample_csv, mode = 'a', header =False)
+        sample_out.to_csv(sample_csv, mode = 'a', header =False)
 
         genes = [gene.split(':') for gene in sim_out.columns[1:]]
         init_mol_count = { gene[0] : {} for gene in genes}
@@ -60,10 +58,10 @@ class CellType:
         #recursive case
         if len(self.children) != 0:
             for cell_type in self.children:
-                cell_type.sim_transition(num_samples, simulator, True, molecule,init_mol_count)
+                cell_type.sim_transition(num_samples, simulator, True, molecule, count_csv, sample_csv, init_mol_count)
                 
         
-    def sim_transition(self, num_samples, simulator, collect_samples, molecule, init_mol_count  = {}):
+    def sim_transition(self, num_samples, simulator, collect_samples, molecule, count_csv, sample_csv, init_mol_count  = {}):
         """
         Params
             num_samples - number of sample reads to output for this cell type
@@ -88,12 +86,12 @@ class CellType:
         if len(init_mol_count) != 0:
             simulator.set_curr_mol_count(init_mol_count)
 
-        f = open(self.count_csv, "w")
+        f = open(count_csv, "w")
         f.truncate()
         f.close()      
         simulator.set_simulation_params(1000, True)
         simulator.simulate(list(np.random.randint(low = 1, high = 1000000, size = 4)))
-        sim_out = pd.read_csv(self.count_csv)
+        sim_out = pd.read_csv(count_csv)
         
         #identify molecules of interest in csv
 
@@ -101,13 +99,13 @@ class CellType:
 
         #collect Traisition samples
         if collect_samples:
-            sim_out = pd.read_csv(self.count_csv)
+            sim_out = pd.read_csv(count_csv)
             sim_out = sim_out.loc[:, ~sim_out.columns.str.contains('^Unnamed')]
             species = [col for col in sim_out.columns if col[-len(molecule):] == molecule]
             samples = np.random.randint(0,len(sim_out.index), size = num_samples)
             sample_out = sim_out.iloc[samples][species]
             sample_out['Cell Type'] = [str(self.lineageName)+'T'] * num_samples
-            sample_out.to_csv(self.sample_csv, mode = 'a', header = False)
+            sample_out.to_csv(sample_csv, mode = 'a', header = False)
         
         #Step 3: Sample num_sample transition cells and store to output and run sim_cell_type
         self.sim_cell_type(num_samples, simulator, molecule)

055b3a0a07c7bd79d358d68c71cb24921fb5dc38
Tarun-Mahajan <tiit828@gmail.com>
2021-12-15 02:30:31 -0600

    For geometric bursts, allow sampled burst value to be zero

--- src/scnnoise/scnnoise.cpp
+++ src/scnnoise/scnnoise.cpp
@@ -1073,7 +1073,7 @@ namespace ScnnoiseInterface {
             double copy_number = gene_copy_number[gene_selected];
             if (burst_size_distribution[gene_selected] == "geometric") {
                 std::geometric_distribution<int> distribution_(double(1.0/(1.0 + burst_size)));
-                gene_burst_sizes[gene_selected] = distribution_(generator) + 1;
+                gene_burst_sizes[gene_selected] = distribution_(generator);
                 stoichio_factor_struct &stoichio_factor_gene =
                     stoichio_factors[gene_selected];
                 stoichio_factor_gene.rxns["transcription"].products_factors["mRNA"] =

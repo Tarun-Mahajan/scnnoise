30b0944b76b83e2c90edceb71abd33d5141ec8cc
Tarun-Mahajan <tiit828@gmail.com>
2024-05-21 15:14:38 -0500

    add license statement

--- src/scnnoise/gillespieSSA.cpp
+++ src/scnnoise/gillespieSSA.cpp
@@ -1,3 +1,12 @@
+/* Copyright (C) 2021 Tarun Mahajan - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the GNU AGPLv3 license.
+ *
+ * You should have received a copy of the GNU AGPLv3 license with
+ * this file. If not, please write to: tarunm3@illinois.edu, or 
+ * visit : https://www.gnu.org/licenses/agpl-3.0.en.html
+ */
+
 #include "scnnoise.hpp"
 #include "gillespieSSA.hpp"
 #include <vector>
--- src/scnnoise/random_generator.hpp
+++ src/scnnoise/random_generator.hpp
@@ -1,3 +1,12 @@
+/* Copyright (C) 2021 Tarun Mahajan - All Rights Reserved
+ * You may use, distribute and modify this code under the
+ * terms of the GNU AGPLv3 license.
+ *
+ * You should have received a copy of the GNU AGPLv3 license with
+ * this file. If not, please write to: tarunm3@illinois.edu, or 
+ * visit : https://www.gnu.org/licenses/agpl-3.0.en.html
+ */
+
 // Generate seeds for random number generator
 #ifndef RANDOM_GEN
 #define RANDOM_GEN
